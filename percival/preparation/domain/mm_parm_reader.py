81267e60af62ca634308b74db42246f854f24497
Yusuke Kawashima <y-kawashima@hoshi.ac.jp>
2020-06-23 22:28:34 +0900

    refactor

--- percival/preparation/domain/mm_parm_reader.py
+++ percival/preparation/domain/mm_parm_reader.py
@@ -2,38 +2,30 @@ import abc
 import os
 import re
 
+
 class MM_param(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def print(self):
+    def print(self, prefix):
         pass
-
     @abc.abstractmethod
     def id(self):
         pass
 
-class VDW(MM_param):
-    atom: str
-    radius: float
-    well_depth: float
 
+class VDW(MM_param):
     def __init__(self, atom: str, radius: float, well_depth: float):
         self.atom = atom.strip().upper()
         self.radius = radius
         self.well_depth = well_depth
 
-    def print(self):
+    def print(self, prefix):
         return f"VDW {self.atom} {self.radius} {self.well_depth}"
 
     def id(self):
         return self.atom
 
+
 class Stretch(MM_param):
-    atom1: str
-    atom2: str
-
-    force_constant: float
-    length: float
-
     def __init__(self, atom1, atom2, force_constant, length):
         self.atom1 = atom1.strip().upper()
         self.atom2 = atom2.strip().upper()
@@ -41,19 +33,14 @@ class Stretch(MM_param):
         self.length = length
         self.atom1, self.atom2 = sorted([self.atom1, self.atom2])
 
-    def print(self):
+    def print(self, prefix):
         return f"HrmStr1 {self.atom1} {self.atom2} {self.force_constant} {self.length}"
 
     def id(self):
         return f"{self.atom1}-{self.atom2}"
 
-class Bend(MM_param):
-    atom1: str
-    atom2: str
-    atom3: str
-    force_constant: float
-    angle: float
 
+class Bend(MM_param):
     def __init__(self, atom1, atom2, atom3, force_constant, angle):
         self.atom1 = atom1.strip().upper()
         self.atom2 = atom2.strip().upper()
@@ -64,31 +51,27 @@ class Bend(MM_param):
         atoms = atoms if atoms[0] <= atoms[2] else list(reversed(atoms))
         self.atom1, self.atom2, self.atom3 = atoms
 
-    def print(self):
+    def print(self, prefix):
         return f"HrmBnd1 {self.atom1} {self.atom2} {self.atom3} {self.force_constant} {self.angle}"
 
     def id(self):
         return f"{self.atom1}-{self.atom2}-{self.atom3}"
 
 
-class ParmReader():
+class ParmReader:
     def __init__(self, path):
         self.file = open(path, "r")
         for line in self.file:
             if len(line) < 5:
                 break
-        self.regexes = {
-            "vdw": re.compile(
-                "^\s*([a-z].)\s+([0-9]+?\.[0-9]+?)\s+([0-9]+?\.[0-9]+?)\s+"
-            ),
-            "str": re.compile(
-            "^\s*([a-z].)-([a-z].)\s+([0-9]+?\.[0-9]+?)\s+([0-9]+?\.[0-9]+?)\s+"
-            ),
-            "bend": re.compile(
-            "^\s*([a-z].)-([a-z].)-([a-z].)\s+([0-9]+?\.[0-9]+?)\s+([0-9]+?\.[0-9]+?)\s+"
-            )
-        }
-        self.dispatch = {
+        self.regexps = {
+            "vdw": re.compile("^\s*([a-z].)\s+([0-9]+?\.[0-9]+?)\s+"
+                              "([0-9]+?\.[0-9]+?)\s+"),
+            "str": re.compile("^\s*([a-z].)-([a-z].)\s+"
+                              "([0-9]+?\.[0-9]+?)\s+([0-9]+?\.[0-9]+?)\s+"),
+            "bend": re.compile("^\s*([a-z].)-([a-z].)-([a-z].)\s+"
+                               "([0-9]+?\.[0-9]+?)\s+([0-9]+?\.[0-9]+?)\s+")}
+        self.parm_dispatcher = {
             "vdw": lambda l: VDW(l[0], l[1], l[2]),
             "str": lambda l: Stretch(l[0], l[1], l[2], l[3]),
             "bend": lambda l: Bend(l[0], l[1], l[2], l[3], l[4])
@@ -100,16 +83,16 @@ class ParmReader():
     def __next__(self):
         for line in self.file:
             i = 0
-            for k, v in self.regexes.items():
+            for k, regexp in self.regexps.items():
                 i += 1
-                res = v.search(line)
+                res = regexp.search(line)
                 if res:
-                    res = res.groups()
-                    res = self.dispatch[k](res)
-                    return [res.id(), res.print()]
+                    atoms = res.groups()
+                    parm = self.parm_dispatcher[k](atoms)
+                    return [parm.id(), parm]
         raise StopIteration
 
 
 if __name__ == '__main__':
-    for i in ParmReader(os.path.dirname(__file__) + "/../service/src/gaff2.dat"):
+    for i in ParmReader(os.path.dirname(__file__) + "/../src/gaff2.dat"):
         print(i)
--- percival/preparation/undef_parm_reader.py
+++ percival/preparation/undef_parm_reader.py
@@ -1,54 +1,53 @@
-from percival.preparation.domain.mm_parm_reader import ParmReader, VDW, Bend, Stretch
+from percival.preparation.domain.mm_parm_reader import ParmReader
 import argparse
 import os
 import re
 
-class UndefParmReader():
+
+class UndefParmReader:
     def __init__(self, path):
         self.file = open(path, "r")
         self.atom_set = set()
         self.atom_set_iter = None
-        self.results = set()
-        self.regex_str = re.compile(
-            "Bondstretch undefined between atoms\s+[0-9]+\s+[0-9]+\s+(.+?)\s*-(.+?)\s+"
-        )
-        self.regex_bend = re.compile(
-            "Angle bend\s+undefined between atoms\s+[0-9]+\s+[0-9]+\s+[0-9]+\s+(.+?)\s*-(.+?)\s*-(.+?)\s+"
-        )
+        self.memo = set()
+        self.regexps = {
+            'str': re.compile("Bondstretch undefined between atoms\s+"
+                              "[0-9]+\s+[0-9]+\s+"
+                              "(.+?)\s*-(.+?)\s+"),
+            'bend': re.compile("Angle bend\s+undefined between atoms\s+"
+                               "[0-9]+\s+[0-9]+\s+[0-9]+\s+"
+                               "(.+?)\s*-(.+?)\s*-(.+?)\s+")}
+        self.sort_dispatcher = {
+            "str": lambda l: sorted(l),
+            "bend": lambda l: l if l[0] <= l[2] else list(reversed(l))
+        }
 
     def __iter__(self):
         return self
 
     def __next__(self):
         for line in self.file:
-            res = self.regex_str.search(line)
-            if res is not None:
-                res = sorted(res.groups())
-                for atom in res:
-                    self.atom_set.add(atom)
-                res = "{}-{}".format(res[0], res[1])
-                if res in self.results:
-                    continue
-                self.results.add(res)
-                return res
-            res = self.regex_bend.search(line)
-            if res is not None:
-                res = res.groups()
-                for atom in res:
-                    self.atom_set.add(atom)
-                res = res if res[0] <= res[2] else list(reversed(res))
-                res = "{}-{}-{}".format(res[0], res[1], res[2])
-                if res in self.results:
-                    continue
-                self.results.add(res)
-                return res
-        if self.atom_set_iter is None:
+            for key, regexp in self.regexps.items():
+                res = regexp.search(line)
+                if res:
+                    atoms = res.groups()
+                    atoms = self.sort_dispatcher[key](atoms)
+                    parm_id = "-".join(atoms)
+                    # Suppress duplicate results
+                    if parm_id in self.memo:
+                        continue
+                    # For VDW parameters
+                    for atom in atoms:
+                        self.atom_set.add(atom)
+                    self.memo.add(res)
+                    return parm_id
+        # Return VDW keys
+        if not self.atom_set_iter:
             self.atom_set_iter = iter(self.atom_set)
-
         for i in self.atom_set_iter:
             return i
-
         raise StopIteration
+
 
 if __name__ == '__main__':
     data = dict([i for i in ParmReader(os.path.dirname(__file__) + "/src/gaff.dat")])
@@ -57,9 +56,8 @@ if __name__ == '__main__':
     p.add_argument("--prefix")
     a = p.parse_args()
     reader = UndefParmReader(a.path)
-    for a in reader:
+    for key in reader:
         try:
-            print(data[a])
+            print(data[key.replace(a.prefix, "")].print(a.prefix))
         except:
-            pass
-
+            print(key)
